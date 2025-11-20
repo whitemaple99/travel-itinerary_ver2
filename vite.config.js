356be4e630078c28dbde04f6133ce70f2ca7e731
whitemaple99 <107122967+whitemaple99@users.noreply.github.com>
2025-11-20 20:41:43 +0900

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-{\rtf1\ansi\ansicpg932\cocoartf2818
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import \{ defineConfig \} from 'vite'\
-import react from '@vitejs/plugin-react'\
-\
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)\
-export default defineConfig(\{\
-  plugins: [react()],\
-\})\
-}
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+export default defineConfig({
+  plugins: [react()],
+})
